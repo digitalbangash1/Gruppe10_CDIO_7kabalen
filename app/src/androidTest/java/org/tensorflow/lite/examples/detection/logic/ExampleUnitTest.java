b868d3c2598820e314d706a55881b5c38b2aab43
OmarEbrahim199 <70900248+OmarEbrahim199@users.noreply.github.com>
2022-06-16 19:34:46 +0200

    JUnit test for (FindMove) method.

---
+++ app/src/androidTest/java/org/tensorflow/lite/examples/detection/logic/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package org.tensorflow.lite.examples.detection.logic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition() {
+        assertEquals(4, 2 + 2);
+        assertNotEquals(21.7,45.2);
+    }
+}
--- app/src/androidTest/java/org/tensorflow/lite/examples/detection/logic/MovesFinderTest.java
+++ app/src/androidTest/java/org/tensorflow/lite/examples/detection/logic/MovesFinderTest.java
@@ -1,10 +1,40 @@
-//package org.tensorflow.lite.examples.detection.logic;
-//
-//import junit.framework.TestCase;
-//
-//public class MovesFinderTest extends TestCase {
-//
-// MovesFinder movesFinder = new MovesFinder();
-// Card2 card1 = new Card2();
-// card1
-//}
+package org.tensorflow.lite.examples.detection.logic;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MovesFinderTest {
+
+    @Test
+    public void findMoves() {
+        ArrayList<String> allCards = new ArrayList<>();
+        allCards.clear();
+        allCards.add("7S");
+        allCards.add("7C");
+        allCards.add("5D");
+        allCards.add("8D");
+        allCards.add("6D");
+        allCards.add("7D");
+        allCards.add("QH");
+        allCards.add("KC");
+
+        MovesFinder movesFinder = new MovesFinder();
+
+        System.out.println("print all pt moves: " + movesFinder.findMoves(allCards));
+
+        ArrayList expectedOutput  =new ArrayList<>(Arrays.asList("7S 8D, 7C 8D, 6D 7S, 6D 7C, QH KC"));
+        ArrayList<CardsMove> methodOutput= movesFinder.findMoves(allCards) ;
+
+        assertEquals("Test",expectedOutput,methodOutput);
+
+
+
+
+    }
+}
